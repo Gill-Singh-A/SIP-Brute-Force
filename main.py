643cb9ce0c1027a5a4d93721278493ce895da0b8
Gill-Singh-A <amansg22@iitk.ac.in>
2024-10-31 06:02:37 +0530

    Implemented Cracking of Digest Based Authentication Data extracted from Packet Capture File

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from hashlib import md5
 from pickle import dump
 from datetime import date
 from base64 import b64decode
+from multiprocessing import Lock, Pool, cpu_count
 from optparse import OptionParser
 from colorama import Fore, Back, Style
 from time import strftime, localtime, time
@@ -26,10 +27,24 @@ def get_arguments(*args):
         parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
     return parser.parse_args()[0]
 
+lock = Lock()
+threads_number = cpu_count()
+
 def calculateDigestResponse(username, password, realm, method, uri, nonce, cnonce, qop, nonce_count):
     hash_1 = md5(f"{username}:{realm}:{password}".encode()).hexdigest()
     hash_2 = md5(f"{method}:{uri}".encode()).hexdigest()
     return md5(f"{hash_1}:{nonce}:{nonce_count}:{cnonce}:{qop}:{hash_2}".encode()).hexdigest()
+def calculateDigestResponse_Handler(details):
+    cracked_authorizations = []
+    for ip, username, realm, method, uri, nonce, cnonce, qop, nonce_count, response in details:
+        for password in arguments.password:
+            calculated_response = calculateDigestResponse(username, password, realm, method, uri, nonce, cnonce, qop, nonce_count)
+            if calculated_response == response:
+                cracked_authorizations.append({"ip": ip, "user": username, "password": password})
+                with lock:
+                    display('+', f"{Back.BLUE}{username}{Back.RESET}:{Back.CYAN}{password}{Back.RESET}@{Back.MAGENTA}{ip}{Back.RESET} => Cracked")
+                break
+    return cracked_authorizations
 
 if __name__ == "__main__":
     arguments = get_arguments(('-i', "--ip", "ip", "File Name of List of IP Addresses (Seperated by ',', either File Name or IP itself)"),
@@ -110,10 +125,10 @@ if __name__ == "__main__":
         successful_logins = []
         for sip_device in sip_devices:
             print(Fore.CYAN + '-'*100 + Fore.RESET)
-            display('*', f"RTSP Device => {Back.MAGENTA}{sip_device['device']}{Back.RESET}")
-            display('*', f"RTSP Client => {Back.MAGENTA}{sip_device['source']}{Back.RESET}")
-            display('*', f"RTSP Device Port => {Back.MAGENTA}{sip_device['device_port']}{Back.RESET}")
-            display('*', f"RTSP Client Port => {Back.MAGENTA}{sip_device['source_port']}{Back.RESET}")
+            display('*', f"SIP Device => {Back.MAGENTA}{sip_device['device']}{Back.RESET}")
+            display('*', f"SIP Client => {Back.MAGENTA}{sip_device['source']}{Back.RESET}")
+            display('*', f"SIP Device Port => {Back.MAGENTA}{sip_device['device_port']}{Back.RESET}")
+            display('*', f"SIP Client Port => {Back.MAGENTA}{sip_device['source_port']}{Back.RESET}")
             display('+', f"Method => {Back.MAGENTA}{sip_device['method']}{Back.RESET}")
             display('+', f"Authorization => {Back.MAGENTA}{sip_device['authorization']}{Back.RESET}")
             if sip_device['authorization'] == "DIGEST":
@@ -123,9 +138,9 @@ if __name__ == "__main__":
                 display(':', f"\t* URI = {Back.MAGENTA}{sip_device['uri']}{Back.RESET}")
                 display(':', f"\t* Response = {Back.MAGENTA}{sip_device['response']}{Back.RESET}")
                 display(':', f"\t* Algorithm = {Back.MAGENTA}{sip_device['algorithm']}{Back.RESET}")
-                display(':', f"\t* CNonce = {Back.MAGENTA}{sip_device['cnonce']}{Back.RESET}")
+                display(':', f"\t* Client Nonce = {Back.MAGENTA}{sip_device['cnonce']}{Back.RESET}")
                 display(':', f"\t* QOP = {Back.MAGENTA}{sip_device['qop']}{Back.RESET}")
-                display(':', f"\t* NC = {Back.MAGENTA}{sip_device['nc']}{Back.RESET}")
+                display(':', f"\t* Nonce Count = {Back.MAGENTA}{sip_device['nc']}{Back.RESET}")
                 sip_device['nc'] = sip_device['nc'].strip()
             else:
                 display(':', f"\t* Username = {Back.MAGENTA}{sip_device['username']}{Back.RESET}")
@@ -135,6 +150,17 @@ if __name__ == "__main__":
         if arguments.capture_file_data:
             with open(arguments.capture_file_data, 'wb') as file:
                 dump(sip_devices, file)
+        pool = Pool(threads_number)
+        threads = []
+        sip_devices = [[sip_device["device"], sip_device["username"], sip_device["realm"], sip_device["method"], sip_device["uri"], sip_device["nonce"], sip_device["cnonce"], sip_device["qop"], sip_device["nc"], sip_device["response"].strip()] for sip_device in sip_devices if sip_device["authorization"] == "DIGEST"]
+        total_sip_devices = len(sip_devices)
+        sip_devices_divisions = [sip_devices[index*total_sip_devices//threads_number: (index+1)*total_sip_devices//threads_number] for index in range(threads_number)]
+        for index, sip_devices_division in enumerate(sip_devices_divisions):
+            threads.append(pool.apply_async(calculateDigestResponse_Handler, (sip_devices_division, )))
+        for thread in threads:
+            successful_logins.extend(thread.get())
+        pool.close()
+        pool.join()
     else:
         ips = []
         for ip_detail in arguments.ip.split(','):
