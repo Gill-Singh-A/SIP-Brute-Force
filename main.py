5897741e4bf30e9baac1140e2768d23523683ea9
Gill-Singh-A <amansg22@iitk.ac.in>
2024-11-02 19:18:15 +0530

    Added SIP Login Logic

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python3
 
+import socket
 from scapy.all import *
 from hashlib import md5
 from pickle import dump
+from random import choice
 from datetime import date
 from base64 import b64decode
 from multiprocessing import Lock, Pool, cpu_count
 from optparse import OptionParser
 from colorama import Fore, Back, Style
-from time import strftime, localtime, time
+from time import strftime, localtime, time, sleep
 
 status_color = {
     '+': Fore.GREEN,
@@ -30,6 +32,29 @@ def get_arguments(*args):
 lock = Lock()
 threads_number = cpu_count()
 
+allowed_characters = "0123456789abcdef"
+numbers = "0123456789"
+ending_string = "\r\n\r\n"
+register_request_data = f"""REGISTER sip:SIP_SERVER SIP/2.0
+Via: SIP/2.0/PROTOCOL SIP_CLIENT
+To: <sip:USERID@SIP_SERVER>
+From: <sip:USERID@SIP_SERVER>
+Call-ID: CALLERID
+CSeq: SEQUENCE REGISTER
+Contact: <sip:USERID@SIP_CLIENT>;expires=3600{ending_string}"""
+register_authorization_data = f"""REGISTER sip:SIP_SERVER SIP/2.0
+Via: SIP/2.0/PROTOCOL SIP_CLIENT
+To: <sip:USERID@SIP_SERVER>
+From: <sip:USERID@SIP_SERVER>
+Call-ID: CALLERID
+CSeq: SEQUENCE REGISTER
+Contact: <sip:USERID@SIP_CLIENT>;expires=3600
+Authorization: Digest username="USERID", realm="REALM", nonce="NONCE", uri="URI", response="RESPONSE", algorithm=MD5, cnonce="CNONCE", qop=QOP, nc=NONCECOUNT{ending_string}"""
+call_id = "kaptaan@PEACOCK"
+server_port = 5060
+client_port = 5060
+protocol = "UDP"
+
 def calculateDigestResponse(username, password, realm, method, uri, nonce, cnonce, qop, nonce_count):
     hash_1 = md5(f"{username}:{realm}:{password}".encode()).hexdigest()
     hash_2 = md5(f"{method}:{uri}".encode()).hexdigest()
@@ -45,6 +70,38 @@ def calculateDigestResponse_Handler(details):
                     display('+', f"{Back.BLUE}{username}{Back.RESET}:{Back.CYAN}{password}{Back.RESET}@{Back.MAGENTA}{ip}{Back.RESET} => Cracked")
                 break
     return cracked_authorizations
+def login(server_ip, client_ip, user, password):
+    t1 = time()
+    try:
+        sequence = 0
+        sip_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM if protocol == "UDP" else socket.SOCK_STREAM)
+        sip_socket.bind((client_ip, client_port)) if protocol == "UDP" else sip_socket.connect((server_ip, server_port))
+        raw_register_request_data = register_request_data.replace("SIP_SERVER", server_ip).replace("PROTOCOL", protocol).replace("SIP_CLIENT", client_ip).replace("USERID", user).replace("CALLERID", call_id).replace("SEQUENCE", str(sequence))
+        sequence += 1
+        sip_socket.sendto(raw_register_request_data.encode(), (server_ip, server_port)) if protocol == "UDP" else sip_socket.sendall(raw_register_request_data.encode())
+        received_data = sip_socket.recvfrom(65535)[0].decode().split('\n') if protocol == "UDP" else sip_socket.recv(65635).decode().split('\n')
+        for line in received_data:
+            if "Authenticate" in line:
+                line = line[line.find("Digest") + len("Digest "):]
+                break
+        response_data = {item.strip().split('=')[0].strip(): item.strip().split('=')[1].strip().replace('"', '') for item in line.split(',')}
+        qop = response_data["qop"]
+        nonce = response_data["nonce"]
+        realm = response_data["realm"]
+        uri = f"sip:{server_ip}"
+        cnonce = ''.join(choice(allowed_characters) for _ in range(8))
+        nc = ''.join(choice(numbers) for _ in range(8))
+        digest_response = calculateDigestResponse(user, password, realm, "REGISTER", uri, nonce, cnonce, qop, nc)
+        raw_register_authorization_data = register_authorization_data.replace("CNONCE", cnonce).replace("NONCECOUNT", nc).replace("SIP_SERVER", server_ip).replace("PROTOCOL", protocol).replace("SIP_CLIENT", client_ip).replace("USERID", user).replace("REALM", realm).replace("NONCE", nonce).replace("URI", uri).replace("RESPONSE", digest_response).replace("QOP", qop).replace("CALLERID", call_id).replace("SEQUENCE", str(sequence))
+        sip_socket.sendto(raw_register_authorization_data.encode(), (server_ip, server_port)) if protocol == "UDP" else sip_socket.sendall(raw_register_authorization_data.encode())
+        received_data = sip_socket.recvfrom(65535)[0].decode() if protocol == "UDP" else sip_socket.recv(65535).decode()
+        sip_socket.close()
+        authorization_status = True if "200 OK" in received_data else False
+        t2 = time()
+        return authorization_status, t2-t1
+    except Exception as error:
+        t2 = time()
+        return error, t2-t1
 
 if __name__ == "__main__":
     arguments = get_arguments(('-i', "--ip", "ip", "File Name of List of IP Addresses (Seperated by ',', either File Name or IP itself)"),
@@ -52,29 +109,65 @@ if __name__ == "__main__":
                               ('-D', "--capture-file-data", "capture_file_data", "Dump Data Extracted from Capture File in Pickle Format (Optional)"),
                               ('-u', "--user", "user", "Username for Brute Force (Seperated by ',', either File Name or User itself)"),
                               ('-p', "--password", "password", "Password For Brute Force (Seperated by ',', either File Name or Password itself)"),
+                              ('-c', "--credentials", "credentials", "Name of File containing Credentials in format ({user}:{password})"),
+                              ('-I', "--call-id", "call_id", f"Call ID (Default={call_id})"),
+                              ('-S', "--server-port", "server_port", f"Server Port (Default={server_port})"),
+                              ('-P', "--client-port", "client_port", f"Client Port (Default={client_port})"),
+                              ('-t', "--protocol", "protocol", f"Protocol to Use (TCP/UDP, Default={protocol})"),
                               ('-w', "--write", "write", "Name of the CSV File for the Successfully Logged In IPs to be dumped (default=current data and time)"))
     if not arguments.write:
         arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
-    if not arguments.password:
-        display('*', f"No {Back.MAGENTA}PASSWORD{Back.RESET} Specified")
-        display(':', f"Setting Password to {Back.MAGENTA}Blank{Back.RESET}")
-        arguments.password = ['']
+    if not arguments.credentials:
+        if not arguments.user and not arguments.capture_file:
+            display('-', f"Please specify {Back.YELLOW}Target Users{Back.RESET} or {Back.YELLOW}Network Packet Capture File{Back.RESET}")
+            exit(0)
+        elif arguments.user:
+            try:
+                with open(arguments.user, 'r') as file:
+                    arguments.user = [user for user in file.read().split('\n') if user != '']
+            except FileNotFoundError:
+                arguments.user = arguments.user.split(',')
+            except:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.user}{Back.RESET}")
+                exit(0)
+            display(':', f"Users Loaded = {Back.MAGENTA}{len(arguments.user)}{Back.RESET}")
+        if not arguments.password:
+            display('*', f"No {Back.MAGENTA}PASSWORD{Back.RESET} Specified")
+            display(':', f"Setting Password to {Back.MAGENTA}Blank{Back.RESET}")
+            arguments.password = ['']
+        else:
+            display(':', f"Loading Passwords from File {Back.MAGENTA}{arguments.password}{Back.RESET}")
+            try:
+                with open(arguments.password, 'rb') as file:
+                    arguments.password = [password for password in file.read().decode(errors="ignore").split('\n') if password != '']
+            except FileNotFoundError:
+                arguments.password = arguments.password.split(',')
+            except Exception as error:
+                display('-', f"Error while Reading File {Back.YELLOW}{arguments.password}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
+                exit(0)
+            display(':', f"Passwords Loaded = {Back.MAGENTA}{len(arguments.password)}{Back.RESET}")
+        if arguments.user:
+            arguments.credentials = []
+            for user in arguments.user:
+                for password in arguments.password:
+                    arguments.credentials.append([user, password])
     else:
         try:
-            with open(arguments.password, 'rb') as file:
-                display(':', f"Loading Passwords from File {Back.MAGENTA}{arguments.password}{Back.RESET}")
-                arguments.password = [password for password in file.read().decode(errors="ignore").split('\n')]
-                display('+', f"Passwords Loaded = {Back.MAGENTA}{len(arguments.password)}{Back.RESET}")
-        except FileNotFoundError:
-            arguments.password = [password for password in arguments.password.split(',')]
-        except OSError:
-            arguments.password = [password for password in arguments.password.split(',')]
-        except MemoryError:
-            display('-', f"File {Back.MAGENTA}{arguments.password}{Back.RESET} too big to load!")
+            with open(arguments.credentials, 'r') as file:
+                arguments.credentials = [[credential.split(':')[0], ':'.join(credential.split(':')[1:])] for credential in file.read().split('\n') if len(credential.split(':')) > 1]
+        except:
+            display('-', f"Error while Reading File {Back.YELLOW}{arguments.credentials}{Back.RESET}")
             exit(0)
-        except Exception as error:
-            display('-', f"Error Loading Passwords from File {Back.YELLOW}{arguments.password}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
-            exit(0)
+    if arguments.call_id:
+        call_id = arguments.call_id
+        register_request_data = register_request_data.replace("CALLERID", call_id)
+        register_authorization_data = register_authorization_data.replace("CALLERID", call_id)
+    if arguments.server_port:
+        server_port = int(arguments.server_port)
+    if arguments.client_port:
+        client_port = int(arguments.client_port)
+    if arguments.protocol == "TCP":
+        protocol = "TCP"
     if not arguments.ip and not arguments.capture_file:
         display('-', "Please Provide a List of IP Addresses or Network Packet Capture File")
         exit(0)
@@ -175,30 +268,9 @@ if __name__ == "__main__":
                     display('+', f"IPs Loaded = {Back.MAGENTA}{len(current_ips)}{Back.RESET}")
             except FileNotFoundError:
                 ips.append(ip_detail)
-            except:
-                display('-', f"Error Loading IPs from File {Back.YELLOW}{arguments.ip}{Back.RESET}")
+            except Exception as error:
+                display('-', f"Error Loading IPs from File {Back.YELLOW}{arguments.ip}{Back.RESET} => {Back.YELLOW}{error}{Back.RESET}")
                 exit(0)
-        if not arguments.user:
-            display('*', f"No {Back.MAGENTA}USER{Back.RESET} Specified")
-            display(':', f"Trying to Find {Back.MAGENTA}Unauthorized Access{Back.RESET}")
-            arguments.user = ['']
-        else:
-            try:
-                with open(arguments.user, 'r') as file:
-                    display(':', f"Loading Users from File {Back.MAGENTA}{arguments.user}{Back.RESET}")
-                    arguments.user = [user for user in file.read().split('\n')]
-                    display('+', f"Users Loaded = {Back.MAGENTA}{len(arguments.user)}{Back.RESET}")
-            except FileNotFoundError:
-                arguments.user = [user for user in arguments.user.split(',')]
-            except OSError:
-                arguments.user = [user for user in arguments.user.split(',')]
-            except:
-                display('-', f"Error Loading Users from File {Back.YELLOW}{arguments.user}{Back.RESET}")
-                exit(0)
-        details = []
-        for user in arguments.user:
-            for password in arguments.password:
-                details.extend([{"ip": ip, "user": user, "password": password} for ip in ips])
     if len(successful_logins) > 0:
         display(':', f"Dumping Successfully Authorized IP Addresses in file {Back.MAGENTA}{arguments.write}{Back.RESET}", start='\n')
         with open(arguments.write, 'w') as file:
